9fdd14e98dd8b73abae5a975fab9c421d6f50858
tildejustin <spacepepper3.14@gmail.com>
2024-06-01 23:53:07 -0400

    2.0.0: rewrite

--- src/main/java/me/voidxwalker/options/extra/mixin/AccessibilityOptionsScreenMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/AccessibilityOptionsScreenMixin.java
@@ -1,34 +1,32 @@
 package me.voidxwalker.options.extra.mixin;
 
-import me.voidxwalker.options.extra.Option;
-import net.minecraft.client.gui.screen.*;
-import net.minecraft.client.gui.screen.options.*;
-import net.minecraft.client.gui.widget.*;
-import net.minecraft.client.options.GameOptions;
-import net.minecraft.text.Text;
+import me.voidxwalker.options.extra.ExtraOptions;
+import net.minecraft.client.gui.screen.options.AccessibilityOptionsScreen;
+import net.minecraft.client.options.Option;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
 
 @Mixin(AccessibilityOptionsScreen.class)
-public class AccessibilityOptionsScreenMixin extends GameOptionsScreen {
-    @Shadow
+public abstract class AccessibilityOptionsScreenMixin {
     @Mutable
+    @Shadow
     @Final
-    private static net.minecraft.client.options.Option[] OPTIONS;
+    private static Option[] OPTIONS;
 
-    static {
-        OPTIONS = new net.minecraft.client.options.Option[]{Option.NARRATOR, Option.SUBTITLES, Option.TEXT_BACKGROUND_OPACITY, Option.TEXT_BACKGROUND, Option.CHAT_OPACITY, Option.CHAT_LINE_SPACING, Option.CHAT_DELAY_INSTANT, Option.AUTO_JUMP, Option.SNEAK_TOGGLED, Option.SPRINT_TOGGLED, Option.extra_options_DISTORTION_EFFECT_SCALE, Option.extra_options_FOV_EFFECT_SCALE, Option.extra_options_MONOCHROME_LOGO};
+    @Inject(method = "<clinit>", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/options/AccessibilityOptionsScreen;OPTIONS:[Lnet/minecraft/client/options/Option;", shift = At.Shift.AFTER))
+    private static void addConfigurationButtons(CallbackInfo ci) {
+        Option[] newOptions = Arrays.copyOf(OPTIONS, OPTIONS.length + 3);
+        newOptions[newOptions.length - 3] = ExtraOptions.DISTORTION_EFFECT_SCALE;
+        newOptions[newOptions.length - 2] = ExtraOptions.FOV_EFFECT_SCALE;
+        newOptions[newOptions.length - 1] = ExtraOptions.AFFECT_BOW;
+        OPTIONS = newOptions;
     }
 
-    public AccessibilityOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
-        super(parent, gameOptions, title);
-    }
-
-    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/options/AccessibilityOptionsScreen;addButton(Lnet/minecraft/client/gui/widget/AbstractButtonWidget;)Lnet/minecraft/client/gui/widget/AbstractButtonWidget;"))
-    public AbstractButtonWidget extra_options_addButton(AccessibilityOptionsScreen instance, AbstractButtonWidget abstractButtonWidget) {
-        if (abstractButtonWidget.x == this.width / 2 - 100 && abstractButtonWidget.y == this.height / 6 + 144) {
-            return this.addButton(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 24 * (14) / 2, 200, 20, ScreenTexts.DONE, (buttonWidget) -> this.client.openScreen(this.parent)));
-        }
-        return this.addButton(abstractButtonWidget);
+    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIIILnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)V"), index = 1)
+    private int moveDoneButtonDown(int original) {
+        return original - 144 + (OPTIONS.length + 1) / 2 * (20 + 4);
     }
 }
--- src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
@@ -1,21 +1,26 @@
 package me.voidxwalker.options.extra.mixin;
 
-import me.voidxwalker.options.extra.GameOptionsAccess;
-import net.minecraft.client.MinecraftClient;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import me.voidxwalker.options.extra.ExtraOptions;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
+@Debug(export = true)
 @Mixin(GameRenderer.class)
-public class GameRendererMixin {
-    @Shadow
-    @Final
-    private MinecraftClient client;
+public abstract class GameRendererMixin {
+    @ModifyExpressionValue(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F"))
+    private float applyDistortionEffectScale(float original) {
+        return original * ExtraOptions.getDistortionEffectScale() * ExtraOptions.getDistortionEffectScale();
+    }
 
-    @Redirect(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 0))
-    public float extra_options_lerp(float delta, float start, float end) {
-        // noinspection DataFlowIssue
-        return MathHelper.lerp(delta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale();
+    // lerping the constant from 70 to 60 in the fraction is the same as lerping the whole fraction from 1 to 60 / 70
+    @ModifyConstant(method = "getFov", constant = @Constant(doubleValue = 60))
+    private double lerpFovChangeInWater(double original) {
+        if (ExtraOptions.affectWater) {
+            return MathHelper.lerp(ExtraOptions.getFovEffectScale(), 70, original);
+        }
+        return original;
     }
 }
