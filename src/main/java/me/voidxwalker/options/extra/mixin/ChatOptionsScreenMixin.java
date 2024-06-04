e64c9b01f767dd936154220aa8afb21aefa1d8cb
tildejustin <spacepepper3.14@gmail.com>
2024-06-04 18:27:44 -0400

    support down to 1.8

--- src/main/java/me/voidxwalker/options/extra/mixin/ChatOptionsScreenMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/ChatOptionsScreenMixin.java
@@ -26,8 +26,14 @@ public abstract class ChatOptionsScreenMixin {
         OPTIONS = newOptions;
     }
 
+    @Dynamic
+    @ModifyConstant(method = "init", constant = {@Constant(intValue = 144), @Constant(intValue = 120)}, require = 1)
+    private int useDynamicOffset(int original) {
+        return 0;
+    }
+
     @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIILjava/lang/String;)V"), index = 2)
     private int moveDoneButtonDown(int original) {
-        return original - 144 + (OPTIONS.length + 1) / 2 * (20 + 4);
+        return original + (OPTIONS.length + 1) / 2 * (20 + 4);
     }
 }
