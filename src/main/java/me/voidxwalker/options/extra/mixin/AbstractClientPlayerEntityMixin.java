6ce71261e25fa9377cd6fcad9cf7979cbdf3c4dd
tildejustin <spacepepper3.14@gmail.com>
2024-06-04 20:18:56 -0400

    clean up

--- src/main/java/me/voidxwalker/options/extra/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/AbstractClientPlayerEntityMixin.java
@@ -2,21 +2,14 @@ package me.voidxwalker.options.extra.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.*;
-import com.mojang.authlib.GameProfile;
 import me.voidxwalker.options.extra.*;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AbstractClientPlayerEntity.class)
-public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
-    public AbstractClientPlayerEntityMixin(World world, GameProfile profile) {
-        super(world, profile);
-    }
-
+public abstract class AbstractClientPlayerEntityMixin {
     // more targeted implementation that only ignores player speed's effect on fov but not bows or creative flight
     @WrapOperation(method = "getSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttributeInstance;getValue()D"))
     private double applyFovEffectScaleSpeedOnly(EntityAttributeInstance instance, Operation<Double> operation) {
@@ -31,7 +24,7 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
     @ModifyReturnValue(method = "getSpeed", at = @At("RETURN"))
     public float applyFovEffectScale(float original) {
         if (ExtraOptions.disableBowFOV) {
-            return MathHelperExt.lerp((float) ExtraOptions.getFovEffectScale(), 1, original);
+            return MathHelperExt.lerp(ExtraOptions.getFovEffectScale(), 1, original);
         }
         return original;
     }
