31e7643ead6a87d700c549fc9b9eea67a9f7a6a5
tildejustin <spacepepper3.14@gmail.com>
2024-06-06 19:47:36 -0400

    refix optifine compat

--- src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
@@ -1,15 +1,23 @@
 package me.voidxwalker.options.extra.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.voidxwalker.options.extra.*;
 import net.minecraft.class_4218;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(class_4218.class)
 public abstract class GameRendererMixin {
-    @ModifyVariable(method = "method_19086", at = @At(value = "STORE", ordinal = 0), ordinal = 1)
-    private float applyDistortionEffectScale(float original) {
-        return (float) (original * ExtraOptions.getDistortionEffectScale() * ExtraOptions.getDistortionEffectScale());
+    @Shadow
+    @Final
+    private MinecraftClient field_20681;
+
+    @ModifyExpressionValue(method = "method_19086", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/ClientPlayerEntity;lastTimeInPortal:F", ordinal = 0))
+    private float applyDistortionEffectScale1(float original, float tickDelta) {
+        float addend = (this.field_20681.player.timeInPortal - this.field_20681.player.lastTimeInPortal) * tickDelta;
+        return (float) (original * ExtraOptions.getDistortionEffectScale() * ExtraOptions.getDistortionEffectScale()
+                + (addend * ExtraOptions.getDistortionEffectScale() * ExtraOptions.getDistortionEffectScale() - addend));
     }
 
     // lerping the constant from 70 to 60 in the fraction is the same as lerping the whole fraction from 1 to 60 / 70
