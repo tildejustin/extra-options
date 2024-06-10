aec69ec9fcfad0978d47f772c2a6d8a36af92c50
tildejustin <spacepepper3.14@gmail.com>
2024-06-10 18:27:03 -0400

    Revert "remove affectWater"
    This reverts commit cc2320ccad2b7070f307b921238a0483298b62c7.

--- src/main/java/me/voidxwalker/options/extra/ExtraOptions.java
+++ src/main/java/me/voidxwalker/options/extra/ExtraOptions.java
@@ -16,6 +16,7 @@ public class ExtraOptions {
     public static DoubleOption FOV_EFFECT_SCALE;
     public static BooleanOption DISABLE_BOW_FOV;
     public static boolean disableBowFOV = false;
+    public static boolean affectWater = true;
     private static float distortionEffectScale = 1;
     private static float fovEffectScale = 1;
 
@@ -68,6 +69,9 @@ public class ExtraOptions {
                     case "disableBowFOV":
                         disableBowFOV = Boolean.parseBoolean(value);
                         break;
+                    case "affectWater":
+                        affectWater = Boolean.parseBoolean(value);
+                        break;
                 }
             });
         }
@@ -81,6 +85,10 @@ public class ExtraOptions {
             writer.write("screenEffectScale:" + distortionEffectScale + "\n");
             writer.write("fovEffectScale:" + fovEffectScale + "\n");
             writer.write("disableBowFOV:" + disableBowFOV + "\n");
+            // semi-hidden option
+            if (!affectWater) {
+                writer.write("affectWater:" + affectWater + "\n");
+            }
         }
     }
 
--- src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
@@ -3,8 +3,9 @@ package me.voidxwalker.options.extra.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.voidxwalker.options.extra.*;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
@@ -14,5 +15,14 @@ public abstract class GameRendererMixin {
             return original;
         }
         return original * ExtraOptions.getDistortionEffectScale() * ExtraOptions.getDistortionEffectScale();
+    }
+
+    // lerping the constant from 70 to 60 in the fraction is the same as lerping the whole fraction from 1 to 60 / 70
+    @ModifyConstant(method = "getFov", constant = @Constant(doubleValue = 60))
+    private double lerpFovChangeInWater(double original) {
+        if (!ExtraOptions.affectWater && !EyeOfEnderCache.shouldDisable()) {
+            return MathHelper.lerp(ExtraOptions.getFovEffectScale(), 70, original);
+        }
+        return original;
     }
 }
