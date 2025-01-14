f820cab3407b8ec5894cd70a625cfb19425c70d2
tildejustin <spacepepper3.14@gmail.com>
2025-01-14 17:32:09 -0500

    use text getter api from https://github.com/KingContaria/SpeedrunAPI/pull/18, on maven local for now

--- src/main/java/me/voidxwalker/options/extra/ExtraOptions.java
+++ src/main/java/me/voidxwalker/options/extra/ExtraOptions.java
@@ -1,13 +1,17 @@
 package me.voidxwalker.options.extra;
 
+import net.minecraft.client.gui.screen.ScreenTexts;
+import net.minecraft.text.*;
 import org.mcsr.speedrunapi.config.api.SpeedrunConfig;
 import org.mcsr.speedrunapi.config.api.annotations.Config;
 
 public class ExtraOptions implements SpeedrunConfig {
     @Config.Numbers.Fractional.Bounds(max = 1)
+    @Config.Text(getter = "getPercentText")
     public static float distortionEffectScale = 1;
 
     @Config.Numbers.Fractional.Bounds(max = 1)
+    @Config.Text(getter = "getPercentText")
     public static float fovEffectScale = 1;
 
     public static boolean controlBowFov = false;
@@ -16,38 +20,9 @@ public class ExtraOptions implements SpeedrunConfig {
 
     public static boolean narratorHotkey = true;
 
-    // public void entrypoint() {
-    //     DISTORTION_EFFECT_SCALE = new DoubleOption(
-    //             /* "options.screenEffectScale" */ "Distortion Effects", 0, 1, 0,
-    //             options -> (double) distortionEffectScale,
-    //             (options, value) -> distortionEffectScale = value.floatValue(),
-    //             (options, option) -> {
-    //                 double d = option.getRatio(option.get(options));
-    //                 MutableText text = option.getDisplayPrefix();
-    //                 return d == 0 ? text.append(ScreenTexts.OFF) : text.append((int) (d * 100) + "%");
-    //             }
-    //     );
-    //     FOV_EFFECT_SCALE = new DoubleOption(
-    //             /* "options.fovEffectScale" */ "FOV Effects", 0, 1, 0,
-    //             options -> Math.pow(fovEffectScale, 2),
-    //             (options, value) -> fovEffectScale = (float) Math.sqrt(value),
-    //             (options, option) -> {
-    //                 double d = option.getRatio(option.get(options));
-    //                 MutableText text = option.getDisplayPrefix();
-    //                 return d == 0 ? text.append(ScreenTexts.OFF) : text.append((int) (d * 100) + "%");
-    //             }
-    //     );
-    //     CONTROL_BOW_FOV = new BooleanOption(
-    //             /* "extra-options.controlBowFov" */ "Control Bow FOV",
-    //             options -> controlBowFov,
-    //             (options, value) -> controlBowFov = value
-    //     );
-    //     CONTROL_SUBMERGED_FOV = new BooleanOption(
-    //             /* "extra-options.controlSubmergedFov" */ "Control Submerged FOV",
-    //             options -> controlSubmergedFov,
-    //             (options, value) -> controlSubmergedFov = value
-    //     );
-    // }
+    private Text getPercentText(float value) {
+        return value == 0 ? ScreenTexts.OFF : new LiteralText((int) (value * 100) + "%");
+    }
 
     @Override
     public String modID() {
