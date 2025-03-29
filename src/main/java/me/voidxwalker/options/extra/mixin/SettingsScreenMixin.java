42b8010a085685d13c3c1abc68a67a353c66a3af
tildejustin <spacepepper3.14@gmail.com>
2025-03-29 15:54:45 -0400

    move button

--- src/main/java/me/voidxwalker/options/extra/mixin/SettingsScreenMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/SettingsScreenMixin.java
@@ -18,7 +18,8 @@ public abstract class SettingsScreenMixin extends Screen {
     @SuppressWarnings("unchecked")
     @Inject(method = "init", at = @At("TAIL"))
     private void addAccessibilitySettingsButton(CallbackInfo ci) {
-        this.buttons.add(new ButtonWidget(106, this.width / 2 - 100, this.height / 6 + 96 - 24 - 6, "Accessibility Settings..."));
+        // not particularly pretty but at least it's not overlapping with anything
+        this.buttons.add(new ButtonWidget(106, this.width / 2 - 100, this.height / 6 + 96 - 24 - 6 + 3, "Accessibility Settings..."));
     }
 
     @ModifyExpressionValue(method = "buttonClicked", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;active:Z"))
